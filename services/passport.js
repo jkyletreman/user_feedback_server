fd4033cc07b823d24744295edf66ee406118e85e
Kyle Treman <jkt290@nyu.edu>
2018-05-20 16:17:32 -0400

    Add user collection from google profile id

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const { googleClientID, googleClientSecret } = require("../config/keys");
+
+const User = mongoose.model('users')
 
 passport.use(
   new GoogleStrategy(
@@ -10,10 +13,8 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(`accessToken, ${accessToken}`);
-      console.log(`refreshToken, ${refreshToken}`);
-      console.log(`profile, ${profile}`);
-
+      // profile id comes from google
+      new User({ googleID: profile.id }).save();
     }
   )
 );
