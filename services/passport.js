eb11da25c17ce5403a9caa599a2249ab9c09030b
Kyle Treman <jkt290@nyu.edu>
2018-05-20 17:27:21 -0400

    Add cookies for oauth via user id

--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,18 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const { googleClientID, googleClientSecret } = require("../config/keys");
 
-const User = mongoose.model('users')
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+});
+
+passport.deserialize((id, done) => {
+  // mongoose query
+  User.findById(id).then(user => {
+    done(null, user)
+  })
+});
 
 passport.use(
   new GoogleStrategy(
@@ -13,12 +24,14 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      // profile id comes from google
+      // profile id comes from google findOne mogoose query
       User.findOne({ googleID: profile.id })
         .then((existingUser) => {
+          // do we have a user?
           if(existingUser) {
             done(null, existingUser);
           } else {
+            // create a new one
             new User({ googleID: profile.id })
               .save()
               .then(user => done(null, user));
