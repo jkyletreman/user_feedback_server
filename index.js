0055e9164dc666e7b687588bb0c20d0cb9b9fd4e
Kyle Treman <jkt290@nyu.edu>
2018-05-19 23:11:31 -0400

    Add google oauth config, route

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const express = require('express');
+const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const { googleClientID, googleClientSecret } = require("./config/keys");
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log("Ready to serve..."))
+app.listen(PORT, console.log("Ready to serve..."));
