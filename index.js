ec246d3b3698cb4434288a2d84a8dd75ccf57b2b
Kyle Treman <jkt290@nyu.edu>
2018-05-20 00:53:38 -0400

    Refractor

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const { googleClientID, googleClientSecret } = require("./config/keys");
+require('./services/passport');
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: googleClientID,
-      clientSecret: googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(`accessToken, ${accessToken}`);
-      console.log(`refreshToken, ${refreshToken}`);
-      console.log(`profile, ${profile}`);
-
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate('google'));
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("Ready to serve..."));
