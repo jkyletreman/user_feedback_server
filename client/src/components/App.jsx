238f6efaea2140e08711ce5581b16cc9caf5fe73
Kyle Treman <jkt290@nyu.edu>
2018-05-22 16:33:37 -0400

    Basic Router structure

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,24 @@
 import React from "react";
-import {} from "react-router-dom";
+import { BrowserRouter, Router } from "react-router-dom";
 
 const Header = () => <h2>Header</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
-const SurveryNew = () => <h2>SurveryNew</h2>;
+const SurveyNew = () => <h2>SurveryNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <p>Hello</p>;
+  return (
+    <div>
+      <BrowserRouter>
+        <React.Fragment>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="surveys/new/" component={SurveyNew} />
+        </React.Fragment>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
