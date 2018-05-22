c4b7db5a37461540397073aeb6207ef5a8178a3a
Kyle Treman <jkt290@nyu.edu>
2018-05-22 16:20:17 -0400

    Add dummy components for Router setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
+import {} from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveryNew = () => <h2>SurveryNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <p>Hello</p>
+  return <p>Hello</p>;
 };
 
 export default App;
