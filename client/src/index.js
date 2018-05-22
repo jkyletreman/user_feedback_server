64dbd94c65110d4c4527f9c3c112e6e8270d6384
Kyle Treman <jkt290@nyu.edu>
2018-05-22 15:56:57 -0400

    Add store prop

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import App from "./components/App";
 const store = createStore(() => [], {}, applyMiddleware());
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
