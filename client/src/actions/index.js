6b0ad062d1ff2b0cf7b96ecc5e077d927352fe8c
Kyle Treman <jkt290@nyu.edu>
2018-05-22 18:49:06 -0400

    Add redux-thunk into fetchUser

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,15 @@
-import axios from 'axios';
-import { fetchUser } from './types';
+import axios from "axios";
+import { fetchUser } from "./types";
 
 const fetchUser = () => {
-  axios.get('/api/current_user');
+  // action creator will return a function
+  // when thunk sees a function returned, it will apply the dispatch
+  return function(dispatch) {
+    axios.get("/api/current_user").then(res =>
+      dispatch({
+        type: FETCH_USER,
+        payload: res
+      })
+    );
+  };
 };
