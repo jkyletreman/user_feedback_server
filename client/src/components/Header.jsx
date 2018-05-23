0f88da44c2f5d3ae89034360ccf08d566388b0ab
Kyle Treman <jkt290@nyu.edu>
2018-05-23 00:26:52 -0400

    Add conditional render based on auth

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,38 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class Header extends Component {
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return "Still deciding";
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return <li><a>Log Out</a></li>
+    }
+  }
   render() {
     return (
       <div>
         <nav>
           <div className="nav-wrapper">
-            <a className='left brand-logo'>Emaily</a>
-            <ul className='right'>
-              <li>
-                <a>Login With Google</a>
-              </li>
-            </ul>
+            <a className="left brand-logo">Emaily</a>
+            <ul className="right">{this.renderContent()}</ul>
           </div>
         </nav>
       </div>
     );
   }
 }
+
+const mapStateToProps = ({ auth }) => {
+  // returns a object with data from the store
+  return { auth };
+};
+
+export default connect(mapStateToProps)(Header);
