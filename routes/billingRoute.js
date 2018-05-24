4fea4a29638ae4290a22174d43a078ad9b58e194
Kyle Treman <jkt290@nyu.edu>
2018-05-24 18:20:29 -0400

    Add ability to add credits to user model and save to db

--- routes/billingRoute.js
+++ routes/billingRoute.js
@@ -3,12 +3,18 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 
 module.exports = app => {
-  app.post("/api/stripe", (req, res) => {
+  app.post("/api/stripe", async (req, res) => {
     stripe.charges.create({
       amount: 500,
       currency: 'usd',
       description: "$5 for 5 credits",
       source: req.body.id
-    })
+    });
+    // passport takes care of attaching the model to the request
+    req.user.credits += 5;
+    // updating the db
+    const user = await req.user.save();
+
+    res.send(user);
   });
 };
