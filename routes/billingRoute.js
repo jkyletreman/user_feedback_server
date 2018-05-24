f8e6cedc72c9eaae7bb29cb1173ecfef98db7ce6
Kyle Treman <jkt290@nyu.edu>
2018-05-24 17:33:20 -0400

    Add to stripe route

--- routes/billingRoute.js
+++ routes/billingRoute.js
@@ -1,3 +1,9 @@
-module.exports = app => {
+const keys = require("../config/keys");
+const stripe = require('stripe')(keys.stripeSecretKey);
 
+
+module.exports = app => {
+  app.post("api/stripe", (req, res) => {
+
+  })
 };
