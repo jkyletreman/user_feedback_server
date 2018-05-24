990193348ae20445a8ac43342c75a136f232c379
Kyle Treman <jkt290@nyu.edu>
2018-05-24 18:31:34 -0400

    Add comments

--- routes/billingRoute.js
+++ routes/billingRoute.js
@@ -4,17 +4,19 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post("/api/stripe", async (req, res) => {
+    //charges user
     stripe.charges.create({
       amount: 500,
       currency: 'usd',
       description: "$5 for 5 credits",
       source: req.body.id
     });
+    // updates model
     // passport takes care of attaching the model to the request
     req.user.credits += 5;
     // updating the db
     const user = await req.user.save();
-
+    
     res.send(user);
   });
 };
