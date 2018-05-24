79072083ab79504f93d51d371fdf5a00b278e8aa
Kyle Treman <jkt290@nyu.edu>
2018-05-23 20:07:09 -0400

    Add name, description to Payments

--- client/src/components/Payments.jsx
+++ client/src/components/Payments.jsx
@@ -4,9 +4,11 @@ import StripeCheckout from 'react-stripe-checkout';
 class Payments extends Component {
   render() {
     debugger;
-    
+
     return (
       <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
